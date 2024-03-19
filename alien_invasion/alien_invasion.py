b21c3ff7ec7ffba4f25be8d83a494e0d24fddfa3
yuelin wen <wenyuelinca@gmail.com>
2024-03-19 19:20:13 -0400

    add ship moving

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -14,22 +14,42 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        self.ship = Ship(self) # self points to the instance of AlienInvasion
+        self.ship = Ship(self)  # self points to the instance of AlienInvasion
 
     def run_game(self):
         # main game start
         while True:  # listen mouse keyboard event
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            # reset background color every flip
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # flip screen every sec
-            pygame.display.flip()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
             self.clock.tick(60)  # use game frame from pygame.time.Clock() set it to 60
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            # detect keydown event
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # right key event
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    # right key event
+                    self.ship.moving_left = True
+
+            # detect key up event, when key release
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+    def _update_screen(self):
+        # reset background color every flip
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # flip screen every sec
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -14,6 +14,16 @@ class Ship:
         # new ship in bottom screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # ship not move when start
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        if self.moving_right:
+            self.rect.x += 1 # moving right add 1 pixel
+        if self.moving_left:
+            self.rect.x -= 1 # moving left minus 1 pixel
+
     def blitme(self):
         # set ship at specific position
         self.screen.blit(self.image, self.rect)
